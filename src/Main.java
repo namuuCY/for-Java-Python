13cb423557e0539f5bd618608db1b623796151b6
namucy <wkdcjfdud13@gmail.com>
2025-12-12 23:52:25 +0900

    stash

--- src/Main.java
+++ src/Main.java
@@ -4,152 +4,405 @@ import java.util.*;
 public class Main {
 
 
-//    https://www.acmicpc.net/problem/17825
-    // 17825 주사위 윷놀이
+//    https://www.acmicpc.net/problem/19235
+    // 19235 모노미노도미노
 
-    static int[] dice = new int[10];
-    static Node[] map = new Node[33];
-    static int maxAns;
+    static int[][] green = new int[6][4];
+    static int[][] blue = new int[4][6];
+    static int ans = 0;
+    static int count1 = 1;
+    static int count2 = 100001;
+    static int count3 = 200001;
+
+    static void debug(int type, int r, int c) {
 
 
-//    static int bt(int count, ) {
-//
-//    }
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        int spr = r+1;
+        int spc = c+1;
 
+        for (int i = 0 ; i < 4 ; i ++) {
+            for (int j = 0; j < 4; j++) {
+                if ((i == r) && (j == c)) {
+                    sb.append(type);
+                    sb.append(" ");
+                    continue;
+                }
+                if (type == 2 && (i == r) && (j == spc)) {
+                    sb.append(type);
+                    sb.append(" ");
+                    continue;
+                }
+                if ((type == 3) && (i == spr) && (j == c)) {
+                    sb.append(type);
+                    sb.append(" ");
+                    continue;
+                }
+                sb.append(0);
+                sb.append(" ");
+            }
 
-    // node는 리스트에 담겨져있음. : 0 이면 시작,
-    static class Node {
-        int score;
-        int current;
-        int nextNode;
-        int subNode;
-
-        Node(int score, int current, int nextNode, int subNode) {
-            this.score = score;
-            this.current = current;
-            this.nextNode = nextNode;
-            this.subNode = subNode;
+            for (int j = 0; j < 6; j++) {
+                sb.append((blue[i][j]== 0)? 0 : blue[i][j]/100000 + 1);
+                sb.append(" ");
+            }
+            sb.append("\n");
         }
 
-        void updateTree(int score, int nextNode, int subNode) {
-            this.score = score;
-            this.nextNode = nextNode;
-            this.subNode = subNode;
+        for (int i = 0 ; i < 6 ; i ++) {
+
+
+            for (int j = 0; j < 4; j++) {
+                sb.append((green[i][j] == 0)? 0 : green[i][j] /100000 +1);
+                sb.append(" ");
+            }
+            sb.append("\n");
         }
 
-        int move(boolean isStart, int count) {
-            if (count == 0) {
-                return this.current;
-            }
-            if (this.current == 21) {
-                return this.current;
-            }
-            if (isStart && this.subNode > 0) {
-                return map[this.subNode].move(false, count - 1);
-            }
-            return map[this.nextNode].move(false, count - 1);
+
+        System.out.println(sb);
+    }
+
+    static void drop(int type, int x, int y) {
+        if (type == 2) {
+            // (x,y) (x,y+1) ->...
+            greenDrop(new int[]{x}, new int[]{y, y+1}, count2);
+            blueDrop(new int[]{x}, new int[]{y, y+1}, count2);
+            count2++;
+        } else if (type == 3) {
+            // (x,y) (x+1, y)
+            greenDrop(new int[]{x, x + 1}, new int[]{y}, count3);
+            blueDrop(new int[]{x, x + 1}, new int[]{y}, count3);
+            count3++;
+        } else {
+            greenDrop(new int[]{x}, new int[]{y}, count1);
+            blueDrop(new int[]{x}, new int[]{y}, count1);
+            count1++;
         }
     }
 
-    static void debug(int[] members) {
-
-        System.out.println(Arrays.toString(members));
-
-    }
-
-    static boolean canMove(int[] members, int currentIdx, int nextMove) {
-        for (int i = 0; i < 4 ; i++) {
-            if (i == currentIdx) continue;
-            if (members[i] == nextMove && (nextMove!= 21)) return false;
-        }
-        return true;
-    }
-
-    static void bt(int count, int ans, int[] origin) {
-        if (count == 10) {
-            if (maxAns < ans) {
-                maxAns = ans;
-//                debug(origin);
+    static void greenDrop(int[] xl, int[] yl, int number) {
+        int maxDepth = 6;
+        for (int y : yl) {
+            int currentDepth = 2;
+            for (int x = 2; x < 6 ; x++) {
+                if (green[x][y] != 0) {
+                    break;
+                }
+                currentDepth ++;
             }
-            return;
+            maxDepth = Math.min(maxDepth, currentDepth);
         }
-        int[] members = origin.clone();
-        for (int i = 0; i < 4; i++) {
-            int nextMove = map[members[i]].move(true, dice[count]);
-            if (canMove(members, i, nextMove)) {
-                members[i] = nextMove;
-                bt(count + 1, ans + map[nextMove].score, members);
-                members[i] = origin[i];
+        if (xl.length > 1) {
+                green[maxDepth -2][yl[0]] = number;
+                green[maxDepth -1][yl[0]] = number;
+        } else {
+            for (int y : yl) {
+                green[maxDepth -1][y] = number;
             }
         }
+        System.out.println("drop되고 난 후");
+        debug(0,0,0);
+    }
+
+    static void blueDrop(int[] xl, int[] yl, int number) {
+        int maxDepth = 6;
+        for (int x : xl) {
+            int currentDepth = 2;
+            for (int y = 2; y < 6 ; y++) {
+                if (blue[x][y] != 0) {
+                    break;
+                }
+                currentDepth ++;
+            }
+            maxDepth = Math.min(maxDepth, currentDepth);
+        }
+        if (yl.length > 1) {
+            blue[xl[0]][maxDepth - 2] = number;
+            blue[xl[0]][maxDepth - 1] = number;
+        } else {
+            for (int x : xl) {
+                blue[x][maxDepth - 1] = number;
+            }
+        }
+    }
 
 
-        // 아래는 기존 실패한 로직
-//        for (int i = 0 ; i < 33; i++) {
-//
-//            if (((bitmask >> i) & 1) != 1) continue;
-//
-//            int currentDice = dice[count];
-//            Node currentNode = map[i];
-//            int nextPoint = currentNode.move(true, currentDice);
-//
-//            // 도착은 중복되도 ㄱㅊ
-//            if (((bitmask >> nextPoint) & 1) == 1 && (nextPoint != 21)) continue;
-//
-//            if (memberCount != 4) {
-//                long nextBm = ((bitmask | (1L << nextPoint)) - (1L << i)) + 1;
-//                bt(nextBm, count + 1, ans + map[nextPoint].score, memberCount + 1);
-//            } else {
-//                long nextBm = ((bitmask | (1L << nextPoint)) - (1L << i));
-//                bt(nextBm, count + 1, ans + map[nextPoint].score, memberCount);
+    static void eraseEnd(int[] gb) {
+        int cnt = gb[0];
+        if (cnt > 0) {
+            for (int i = 5; i >= cnt; i--) {
+                for (int j = 0; j < 4; j++) {
+                    green[i][j] = green[i - cnt][j];
+                }
+            }
+            // 윗부분 비우기
+            for (int i = 0; i < cnt; i++) {
+                for (int j = 0; j < 4; j++) {
+                    green[i][j] = 0;
+                }
+            }
+        }
+
+        // 2. Blue 처리
+
+        cnt = gb[1];
+        if (cnt > 0) {
+            for (int j = 5; j >= cnt; j--) {
+                for (int i = 0; i < 4; i++) {
+                    blue[i][j] = blue[i][j - cnt];
+                }
+            }
+            for (int j = 0; j < cnt; j++) {
+                for (int i = 0; i < 4; i++) {
+                    blue[i][j] = 0;
+                }
+            }
+        }
+    }
+
+
+//    static void eraseEnd(int[] gb) {
+//        for (int r = 5 ; r > 5-gb[0] ; r--) {
+//            for (int c = 0; c  < 4; c++) {
+//                green[r][c] = 0;
 //            }
 //        }
+//
+//        for (int c = 5 ; c > 5-gb[1] ; c--) {
+//            for (int r = 0; r  < 4; r++) {
+//                blue[r][c] = 0;
+//            }
+//        }
+//    }
+
+    static boolean eraseWithScore() {
+        boolean isMore = false;
+        for (int i = 0; i < 6 ; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (green[i][j] == 0) break;
+                if (j == 3) {
+                    debug(0, 0, 0);
+                    System.out.println("이상태에서 그린에 " + i + " 행 제거됨");
+                    for (int k = 0; k < 4; k++) {
+                        green[i][k] = 0;
+                    }
+                    ans ++;
+                    isMore = true;
+                }
+            }
+        }
+
+        for (int c = 0; c < 6 ; c++) {
+            for (int r = 0; r < 4; r++) {
+                if (blue[r][c] == 0) break;
+                if (r == 3) {
+                    for (int k = 0; k < 4; k++) {
+                        blue[k][c] = 0;
+                    }
+                    System.out.println("블루에 " + c + " 열 제거됨");
+                    ans ++;
+                    isMore = true;
+                }
+            }
+        }
+
+        return isMore;
     }
+
+    static void press() {
+        // 각각 r, c 맨 오른쪽부터
+        for (int r = 4; r >= 0 ; r--) {
+            for (int c = 0; c < 4; c++) {
+                if (green[r][c] == 0) continue;
+                int temp = green[r][c];
+                if (green[r][c] / 100000 == 1) {
+                    // type 2
+                    // // (x,y) (x,y+1)
+                    if ((c + 1 < 4) && green[r][c + 1] == green[r][c]) {
+                        green[r][c] = 0;
+                        green[r][c+1] = 0;
+                        int gap = 10;
+                        for (int i = 0; i < 2; i++) {
+                            int dist = 0;
+                            while (((r+dist+1) <= 5) && green[r + dist + 1][c+i] == 0) {
+                                dist++;
+                            }
+                            gap = Math.min(gap, dist);
+                        }
+                        green[r + gap][c] = temp;
+                        green[r + gap][c+1] = temp;
+                        continue;
+                    }
+
+                    green[r][c] = 0;
+                    int dist = 0;
+                    while (((r+dist+1) <= 5) && green[r + dist + 1][c] == 0) {
+                        dist++;
+                    }
+                    green[r+dist][c] = temp;
+
+                } else if (green[r][c] / 100000 == 2) {
+                    // 2개 챙겨야 할 때
+
+                    if ((r - 1 >= 0) && green[r-1][c] == green[r][c]) {
+                        green[r][c] = 0;
+                        green[r-1][c] = 0;
+                        int dist = 0;
+                        while (((r+dist+1) <= 5) && green[r + dist + 1][c] == 0) {
+                            dist++;
+                        }
+                        green[r + dist][c] = temp;
+                        green[r + dist -1][c] = temp;
+                        continue;
+                    }
+
+                    green[r][c] = 0;
+                    int dist = 0;
+                    while (((r+dist+1) <= 5) && green[r + dist + 1][c] == 0) {
+                        dist++;
+                    }
+                    green[r+dist][c] = temp;
+                    // 1개만
+                } else {
+                    green[r][c] = 0;
+                    int dist = 0;
+                    while (((r+dist+1) <= 5) && green[r + dist + 1][c] == 0) {
+                        dist++;
+                    }
+                    green[r+dist][c] = temp;
+                }
+            }
+        }
+
+        for (int c = 4; c >= 0 ; c--) {
+            for (int r = 0; r < 4; r++) {
+                if (blue[r][c] == 0) continue;
+                int temp = blue[r][c];
+                if (blue[r][c] / 100000 == 2) {
+                    // type 3
+                    // // (x,y) (x,y+1)
+
+                    if ((r + 1 < 4) && blue[r + 1][c] == blue[r][c]) {
+                        blue[r][c] = 0;
+                        blue[r+1][c] = 0;
+                        int gap = 10;
+                        for (int i = 0; i < 2; i++) {
+                            int dist = 0;
+                            while (((c + dist + 1) <= 5) && blue[r +i][c + dist + 1] == 0) {
+                                dist++;
+                            }
+                            gap = Math.min(gap, dist);
+                        }
+                        blue[r][c + gap] = temp;
+                        blue[r+1][c+ gap] = temp;
+                        continue;
+                    }
+
+                    blue[r][c] = 0;
+                    int dist = 0;
+                    while (((c+dist+1) <= 5) && blue[r][c+dist+1] == 0) {
+                        dist++;
+                    }
+                    blue[r][c+dist] = temp;
+
+                } else if (blue[r][c] / 100000 == 1) {
+                    // 2개 챙겨야 할 때
+
+                    if ((c - 1 >= 0) && blue[r][c - 1] == blue[r][c]) {
+                        blue[r][c] = 0;
+                        blue[r][c-1] = 0;
+                        int dist = 0;
+                        while (((c+dist+1) <= 5) && blue[r ][c+ dist + 1] == 0) {
+                            dist++;
+                        }
+                        blue[r][c + dist] = temp;
+                        blue[r][c + dist -1] = temp;
+                        continue;
+                    }
+
+                    blue[r][c] = 0;
+                    int dist = 0;
+                    while (((c+dist+1) <= 5) && blue[r][c + dist + 1] == 0) {
+                        dist++;
+                    }
+                    blue[r][c+dist] = temp;
+                    // 1개만
+                } else {
+                    blue[r][c] = 0;
+                    int dist = 0;
+                    while (((c+dist+1) <= 5) && blue[r][c + dist + 1] == 0) {
+                        dist++;
+                    }
+                    blue[r][c+dist] = temp;
+                }
+            }
+        }
+        System.out.println("press되고 난 후");
+    }
+
+    // 제거해야할 줄 수 알려줌
+    // int[] = {green, blue}
+    static int[] checkShell() {
+        int[] gb = new int[2];
+
+        for (int r = 0 ; r < 2 ; r++) {
+            for (int c = 0; c < 4 ; c++) {
+                if (green[r][c] == 0) continue;
+                gb[0]++;
+                break;
+            }
+        }
+
+        for (int c = 0 ; c < 2 ; c++) {
+            for (int r = 0; r < 4 ; r++) {
+                if (blue[r][c] == 0) continue;
+                gb[1]++;
+//                System.out.println(gb[1]);
+
+                break;
+            }
+        }
+        return gb;
+    }
+
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0 ; i < 10; i++) {
-            dice[i] = Integer.parseInt(st.nextToken());
+        while (N -- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int type = Integer.parseInt(st.nextToken());
+            int dropR = Integer.parseInt(st.nextToken());
+            int dropC = Integer.parseInt(st.nextToken());
+            drop(type, dropR, dropC);
+
+
+            while (true) {
+                press();
+                if (!eraseWithScore()) break;
+            }
+
+            int[] gb = checkShell();
+            eraseEnd(gb);
+
+            debug(type, dropR, dropC);
         }
 
-        for (int i = 0; i < 33; i++) {
-            map[i] = new Node(i * 2, i, i+1, -1);
+        System.out.println(ans);
+
+        int block = 0;
+        for (int i = 0; i< 6 ;i++) {
+            for (int j = 0 ; j < 4 ; j++) {
+                block += (green[i][j]!=0) ? 1:0;
+                block += (blue[j][i]!=0) ? 1:0;
+            }
         }
+        System.out.println(block);
 
 
-        // 예외 케이스만 업데이트
-        map[5].updateTree(10, 6, 22);
-        map[10].updateTree(20, 11, 26);
-        map[15].updateTree(30, 16, 28);
-        map[21].updateTree(0, 21, -1);
-        map[22].updateTree(13, 23, -1);
-        map[23].updateTree(16, 24, -1);
-        map[24].updateTree(19, 25, -1);
-
-        map[25].updateTree(25, 31, -1);
-        map[26].updateTree(22, 27, -1);
-        map[27].updateTree(24, 25, -1);
-        map[28].updateTree(28, 29, -1);
-        map[29].updateTree(27, 30, -1);
-        map[30].updateTree(26, 25, -1);
-        map[31].updateTree(30, 32, -1);
-        map[32].updateTree(35, 20, -1);
-
-//
-//        for (int j = 0 ; j < 33 ; j++) {
-//
-//            System.out.println(j + "일때" + "아래의 5칸 움직임");
-//            StringBuilder sb = new StringBuilder();
-//            for (int i = 1; i <6 ; i++) {
-//                sb.append(map[map[j].move(true, i)].score);
-//                sb.append(" ");
-//            }
-//            System.out.println(sb);
-//
-//        }
-        bt(0, 0, new int[]{0, 0, 0, 0});
-        System.out.println(maxAns);
     }
 }

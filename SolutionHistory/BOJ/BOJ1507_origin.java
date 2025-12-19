e4ca1343a25a12cb9f9c15417cf2248aaf2e5eaa
namucy <wkdcjfdud13@gmail.com>
2025-12-19 23:43:21 +0900

    BOJ1507) 플로이드 알고리즘 응용한 역추적
    - 삼각부등식 생각

---
+++ SolutionHistory/BOJ/BOJ1507_origin.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// https://www.acmicpc.net/problem/1507
+	// 1507 궁금한 민호
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] dist = new int[N][N];
+
+		boolean[][] isRedundant = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				dist[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+
+					if (i == k || j == k || i == j) continue;
+
+					if (dist[i][j] > dist[i][k] + dist[k][j]) {
+						System.out.println(-1);
+						return;
+					}
+
+					if (dist[i][j] == dist[i][k] + dist[k][j]) {
+						isRedundant[i][j] = true;
+					}
+				}
+			}
+		}
+
+		int result = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (!isRedundant[i][j]) {
+					result += dist[i][j];
+				}
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}

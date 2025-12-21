b13f1d31dd677f08e9b38077d7fb034447978b9e
namucy <wkdcjfdud13@gmail.com>
2025-12-21 23:04:10 +0900

    BOJ23258) 밤편지
    - 플로이드 알고리즘 응용
    - 플로이드 알고리즘의 의의 : 가장 바깥 루프의 k(pass)가 거쳐가는 모든 경우를 의미
    - 이거에 맞춰서 쿼리를 정렬하고, 필요한 쿼리부터 계산

---
+++ SolutionHistory/BOJ/BOJ23258.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// https://www.acmicpc.net/problem/23258
+	// 23258 밤편지
+
+	// 플로이드 알고리즘 : 다 돌렸을때는 전역 최단거리라서 2등 정보가 누락됨
+	// 쿼리를 C에 대해서 정렬시켜서, 해당 쿼리가 돌아갔을 때 계산할 것
+	static int N;
+	static int Q;
+	static int INF = 0x3f3f3f3f;
+	static int[][] dist;
+	static int[] ans;
+
+	static class Query {
+		int start;
+		int end;
+		int index;
+
+		Query(int start, int end, int index) {
+			this.start = start;
+			this.end = end;
+			this.index = index;
+		}
+	}
+
+	static void command(int C, List<Query>[] queries) {
+		for (Query q : queries[C]) {
+			ans[q.index] = (dist[q.start][q.end] >= INF)
+					? -1
+					: dist[q.start][q.end];
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
+
+		dist = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			Arrays.fill(dist[i], INF);
+			dist[i][i] = 0;
+		}
+		ans = new int[Q];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				int currentDist = Integer.parseInt(st.nextToken());
+				if (i == j) continue;
+				if (currentDist == 0) continue;
+				dist[i][j] = currentDist;
+			}
+		}
+
+		List<Query>[] queries = new ArrayList[N + 2];
+		for (int i = 1; i <= N + 1; i++) {
+			queries[i] = new ArrayList<>();
+		}
+
+		for (int idx = 0 ; idx < Q; idx++) {
+			st = new StringTokenizer(br.readLine());
+
+			int C = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			queries[C].add(new Query(start, end, idx));
+		}
+		command(1, queries);
+
+		for (int pass = 1; pass <= N; pass++) {
+
+			for (int s = 1; s <= N; s++) {
+				for (int e = 1; e <= N; e++) {
+					if (dist[s][pass] + dist[pass][e] >= dist[s][e]) continue;
+					dist[s][e] = dist[s][pass] + dist[pass][e];
+				}
+			}
+			command(pass + 1, queries);
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int a : ans) {
+			sb.append(a);
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,96 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // https://www.acmicpc.net/problem/14244
-    // 14244 트리만들기
+    // https://www.acmicpc.net/problem/23258
+    // 23258 밤편지
 
+    // 플로이드 알고리즘 : 다 돌렸을때는 전역 최단거리라서 2등 정보가 누락됨
+    // 쿼리를 C에 대해서 정렬시켜서, 해당 쿼리가 돌아갔을 때 계산할 것
+    static int N;
+    static int Q;
+    static int INF = 0x3f3f3f3f;
+    static int[][] dist;
+    static int[] ans;
 
-    //    n과 m이 주어진다. (3 ≤ n ≤ 50, 2 ≤ m ≤ n-1)
+    static class Query {
+        int start;
+        int end;
+        int index;
 
+        Query(int start, int end, int index) {
+            this.start = start;
+            this.end = end;
+            this.index = index;
+        }
+    }
 
-
+    static void command(int C, List<Query>[] queries) {
+        for (Query q : queries[C]) {
+            ans[q.index] = (dist[q.start][q.end] >= INF)
+                    ? -1
+                    : dist[q.start][q.end];
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 리프노드만큼 오른쪽에 추가
-        // 차액 - 1 만큼 아래에 추가
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+        ans = new int[Q];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int currentDist = Integer.parseInt(st.nextToken());
+                if (i == j) continue;
+                if (currentDist == 0) continue;
+                dist[i][j] = currentDist;
+            }
+        }
+
+        List<Query>[] queries = new ArrayList[N + 2];
+        for (int i = 1; i <= N + 1; i++) {
+            queries[i] = new ArrayList<>();
+        }
+
+        for (int idx = 0 ; idx < Q; idx++) {
+            st = new StringTokenizer(br.readLine());
+
+            int C = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            queries[C].add(new Query(start, end, idx));
+        }
+        command(1, queries);
+
+        for (int pass = 1; pass <= N; pass++) {
+
+            for (int s = 1; s <= N; s++) {
+                for (int e = 1; e <= N; e++) {
+                    if (dist[s][pass] + dist[pass][e] >= dist[s][e]) continue;
+                    dist[s][e] = dist[s][pass] + dist[pass][e];
+                }
+            }
+            command(pass + 1, queries);
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        for (int leaf = 1; leaf <= m ; leaf++) {
-            sb.append(0);
-            sb.append(" ");
-            sb.append(leaf);
+        for (int a : ans) {
+            sb.append(a);
             sb.append("\n");
         }
 
-        int nextNode = 1;
-
-        for (int currentNode = m + 1; currentNode < n; currentNode++) {
-            sb.append(nextNode);
-            sb.append(" ");
-            sb.append(currentNode);
-            sb.append("\n");
-            nextNode = currentNode;
-        }
-
-        System.out.println(sb);
-
+        bw.write(sb.toString());
+        bw.flush();
     }
 
 }

49f0b9d3ec0e1b9f51d1ae51c98b40a8bfad5847
namucy <wkdcjfdud13@gmail.com>
2026-01-12 23:17:36 +0900

    stash

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,68 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    // https://www.acmicpc.net/problem/19951
-    // 태상이의 훈련소 생활
+    // https://www.acmicpc.net/problem/1029
+    // 그림 교환
+
+    // DFS, BFS는 불가능할것 -> N! - 14! 하면 10억초과
+    // 그리디 접근 방법? 케이스가 많아서 불가능해보임
+    // 플로이드 - 워셜? 얘네는 최단 경로를 출력하므로 불가능해보임
+    // 비트마스킹까진 생각났는데... 2^15 = 32000 라 가능해보이고
+    // DP[마지막 구매자][여태까지 총 구매자 비트마스킹] 15 * (2 ^ 15) = 50만정도?
+    // 밑에서부터 채울거지? 어떻게 채울거야
+    // 우선 1부터 채움.
+
+
+    static class Transaction {
+        int currentPrice;
+        int transactionCount;
+
+        Transaction(int currentPrice, int transactionCount) {
+            this.currentPrice = currentPrice;
+            this.transactionCount = transactionCount;
+        }
+
+        Transaction compareAndUpsert() {
+
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int groundSize = Integer.parseInt(st.nextToken());
-        int querySize = Integer.parseInt(st.nextToken());
-        // 1-idx
-        int[] heights = new int[groundSize + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1 ; i <= groundSize ; i++) {
-            heights[i] = Integer.parseInt(st.nextToken());
+
+        int traderSize = Integer.parseInt(br.readLine());
+        int[][] expectedPrice = parseInput(traderSize, br);
+
+        int[][] DP = new int[traderSize][(1 << traderSize)];
+
+    }
+
+    private static int[][] parseInput(int traderSize, BufferedReader br) throws IOException {
+
+        int[][] expectedPrice = new int[traderSize][traderSize];
+        for (int row = 0 ; row < traderSize ; row++) {
+            char[] currentInputChars = br.readLine().strip().toCharArray();
+            for (int column = 0 ; column < traderSize ; column ++) {
+                expectedPrice[row][column] = (char) currentInputChars[column] - (char) '0';
+            }
         }
 
-        // 각각의 ground에 대해서 쿼리 하나하나씩 작업한다면 시간복잡도 최대 100억. 시간 내 불가
-        // prefixSum 도입
-        int[] prefixes = new int[groundSize + 2];
+        return expectedPrice;
+    }
 
-        while (querySize -- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int startIdx = Integer.parseInt(st.nextToken());
-            int endIdx = Integer.parseInt(st.nextToken()) + 1;
-            int diff = Integer.parseInt(st.nextToken());
+    private static void updateDp(int traderSize, int[][] expectedPrice, int[][] DP) {
+        DP[0][1] = 1;
 
-            prefixes[startIdx] += diff;
-            prefixes[endIdx] -= diff;
+        for (int currentTraderBitmask = 1 ; currentTraderBitmask < (1 << traderSize) ; currentTraderBitmask ++) {
+            for (int nextTrader = 1; nextTrader < traderSize ; nextTrader++) {
+                // 현재 비트마스크 기반으로 방문하지 않은애들에 대해서 스타트
+                if (( (currentTraderBitmask >> nextTrader) & 1 ) == 1) continue;
+                // 가격 비교 후 업데이트 X 조건일 경우 생각
+
+                // 업데이트 시에는 무조건 Math.max를 통해서 비교해야함.
+                // 만약 같은데, 기존 프라이스보다 낮은 케이스
+            }
         }
-
-        int currentDiff = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < groundSize + 1; i ++) {
-            currentDiff += prefixes[i];
-            heights[i] += currentDiff;
-            sb.append(heights[i]);
-            sb.append(" ");
-        }
-
-        System.out.println(sb);
     }
 }

5ccc8c48c3aac0ec0bc7ccaee2e69cd80b26c39c
namucy <wkdcjfdud13@gmail.com>
2026-03-07 13:16:53 +0900

    BOJ1463)

---
+++ SolutionHistory/BOJ/BOJ1463.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	// https://www.acmicpc.net/problem/1463
+
+	static int[] dp ;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		int n = Integer.parseInt( br.readLine() );
+		dp = new int[n + 1];
+		Arrays.fill(dp, -1);
+
+		int answer = dfs(n);
+
+		System.out.println(answer);
+	}
+
+	private static int dfs(int n) {
+		if (n == 1) return 0;
+
+		if (dp[n] != -1) return dp[n];
+
+		int answer = 10_000_000;
+
+		if (n % 3 == 0) {
+			answer = Math.min(answer, dfs(n / 3) + 1);
+		}
+
+		if (n % 2 == 0) {
+			answer = Math.min(answer, dfs(n / 2) + 1);
+		}
+
+		answer = Math.min(answer, dfs(n - 1) + 1);
+
+		return dp[n] = answer;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,37 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    // https://www.acmicpc.net/submit/24417
+    // https://www.acmicpc.net/problem/1463
 
+    static int[] dp ;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        int n = Integer.parseInt( br.readLine() );
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
 
-        int mod = 1_000_000_007;
+        int answer = dfs(n);
 
-        // 코드 2 실행 횟수: n - 2
-        // n의 최댓값이 2억이므로 10억 7보다 작아 별도의 모듈러 연산이 필요 없습니다.
-        int code2Count = n - 2;
+        System.out.println(answer);
+    }
 
-        // 코드 1 실행 횟수: n번째 피보나치 수 구하기 (O(N) 알고리즘, O(1) 공간 복잡도)
-        int prev2 = 1; // f[1]
-        int prev1 = 1; // f[2]
-        int current = 1;
+    private static int dfs(int n) {
+        if (n == 1) return 0;
 
-        for (int i = 3; i <= n; i++) {
-            current = (prev1 + prev2) % mod;
-            prev2 = prev1;
-            prev1 = current;
+        if (dp[n] != -1) return dp[n];
+
+        int answer = 10_000_000;
+
+        if (n % 3 == 0) {
+            answer = Math.min(answer, dfs(n / 3) + 1);
         }
 
-        System.out.println(current + " " + code2Count);
+        if (n % 2 == 0) {
+            answer = Math.min(answer, dfs(n / 2) + 1);
+        }
+
+        answer = Math.min(answer, dfs(n - 1) + 1);
+
+        return dp[n] = answer;
     }
 }

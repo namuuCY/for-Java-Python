c9504a89292a90c89c42010d72fa2e3f7d14a6bb
namucy <wkdcjfdud13@gmail.com>
2026-03-11 22:25:17 +0900

    BOJ23978)

---
+++ SolutionHistory/BOJ/BOJ23978.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	// https://www.acmicpc.net/problem/23978
+
+	static int N;
+	static long K;
+	static int[] days;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Long.parseLong(st.nextToken());
+//        System.out.println(K);
+
+		days = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0 ; i < N; i++) {
+			days[i] = Integer.parseInt(st.nextToken());
+		}
+
+		long answer = binarySearch();
+
+		System.out.println(answer);
+
+	}
+	// 최소의 long X 를 찾고 싶다.
+	private static long binarySearch() {
+		long left = 1;
+		long right = 10_000_000_000L;
+
+		while (left < right) {
+			long mid = left + (right - left) / 2;
+			if (isPossible(mid)) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		return left;
+	}
+
+	// a 부터 b 까지의 합 -> (a + b /) 2 * (b - a)
+	// 1 2 4
+	// 10 억
+	private static boolean isPossible(long current) {
+		long answer = 0;
+
+		for (int i = 0 ; i < N - 1; i ++) {
+			if (days[i + 1] - days[i] == 1) {
+				answer += current;
+			} else {
+				int gap = Math.min(days[i + 1] - days[i], (int) current);
+				// 숫자 차이가 current 보다 작으면 문제가 생겼네
+				answer += ((current + current - (gap - 1)) * (gap)) / 2;
+			}
+//            System.out.println("지금 current : " + current +  " 일 때 answer : " + answer);
+			if (answer >= K) return true;
+		}
+//        System.out.println("지금 current : " + current +  " 일 때 answer : " + answer);
+		answer += (current + 1) * (current) / 2; // 5억 * 5억 이면
+
+		return answer >= K;
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,71 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    // https://www.acmicpc.net/problem/9252
+
+    // https://www.acmicpc.net/problem/23978
 
     static int N;
-    static int M;
-    static int[][] DP;
+    static long K;
+    static int[] days;
 
     public static void main(String[] args) throws IOException {
-        // 두 문자는 최대 1000 글자
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] firstWord = br.readLine().strip().toCharArray();
-        char[] secondWord = br.readLine().strip().toCharArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Long.parseLong(st.nextToken());
+//        System.out.println(K);
 
-        N = firstWord.length;
-        M = secondWord.length;
-
-        DP = new int[N + 1][M + 1];
-
+        days = new int[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0 ; i < N; i++) {
-            for (int j = 0 ; j < M; j++) {
-                if (firstWord[i] == secondWord[j]) {
-                    DP[i + 1][j + 1] = DP[i][j] + 1;
-                } else {
-                    DP[i + 1][j + 1] = Math.max(DP[i][j + 1], DP[i + 1][j]);
-                }
-            }
+            days[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(DP[N][M]);
+        long answer = binarySearch();
 
-        // DP 테이블 기반 역추적
-        if (DP[N][M] == 0) return;
+        System.out.println(answer);
 
-        int i = N;
-        int j = M;
-        StringBuilder sb = new StringBuilder();
-
-        while (i > 0 && j > 0) {
-            // 같으면 대각선 위로
-            if (firstWord[i - 1] == secondWord[j - 1]) {
-                sb.append(firstWord[i - 1]);
-                i -- ;
-                j -- ;
-            }
-            // 값이 같은 DP 테이블로 추적
-            else if (DP[i][j] == DP[i - 1][j]) {
-                i --;
-            }
-            else if (DP[i][j] == DP[i][j - 1]) {
-                j --;
-            }
-        }
-
-        System.out.println(sb.reverse());
     }
+    // 최소의 long X 를 찾고 싶다.
+    private static long binarySearch() {
+        long left = 1;
+        long right = 10_000_000_000L;
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (isPossible(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    // a 부터 b 까지의 합 -> (a + b /) 2 * (b - a)
+    // 1 2 4
+    // 10 억
+    private static boolean isPossible(long current) {
+        long answer = 0;
+
+        for (int i = 0 ; i < N - 1; i ++) {
+            if (days[i + 1] - days[i] == 1) {
+                answer += current;
+            } else {
+                int gap = Math.min(days[i + 1] - days[i], (int) current);
+                // 숫자 차이가 current 보다 작으면 문제가 생겼네
+                answer += ((current + current - (gap - 1)) * (gap)) / 2;
+            }
+//            System.out.println("지금 current : " + current +  " 일 때 answer : " + answer);
+            if (answer >= K) return true;
+        }
+//        System.out.println("지금 current : " + current +  " 일 때 answer : " + answer);
+        answer += (current + 1) * (current) / 2; // 5억 * 5억 이면
+
+        return answer >= K;
+    }
+
 }
